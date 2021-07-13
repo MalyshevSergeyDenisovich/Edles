8f3bd98fdd9d7692dbb3cc54804a2e13bceb6f90
Rostov <rostov.90.100@gmail.com>
2021-07-13 04:35:27 +0300

    "feature_items,inventory,items[]started"

---
+++ Assets/Bin/Units/Staff/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Bin.Units.Items;
+
+namespace Bin.Units.Staff
+{
+    public class Inventory
+    {
+        private readonly List<BaseItem> _items;
+
+        protected Inventory()
+        {
+            _items = new List<BaseItem>();
+        }
+
+
+        public void Put(BaseItem item)
+        {
+            _items.Add(item);
+        }
+
+        public void Remove(BaseItem item)
+        {
+            _items.Remove(item);
+        }
+    }
+}
