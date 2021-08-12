1d36d4421a173ac7929490afff57f7a468bd2569
Rostov <rostov.90.100@gmail.com>
2021-08-12 17:58:04 +0300

    "feature_Pathfinding_smooth weights"

--- Assets/Bin/Map/Grid.cs
+++ Assets/Bin/Map/Grid.cs
@@ -11,6 +11,7 @@ namespace Bin.Map
         [SerializeField] private Vector2 gridWorldSize;
         [SerializeField] private float nodeRadius;
         [SerializeField] private TerrainType[] walkableRegions;
+        [SerializeField] private int obstacleProximityPenalty = 10;
         private LayerMask _walkableLayerMask;
         private Dictionary<int, int> _walkableRegionsDictionary = new Dictionary<int, int>();
         
@@ -18,6 +19,10 @@ namespace Bin.Map
 
         private float _nodeDiameter;
         private int _gridSizeX, _gridSizeY;
+        
+        
+        private int _penaltyMin = int.MaxValue;
+        private int _penaltyMax = int.MinValue;
         private void Awake()
         {
             _nodeDiameter = nodeRadius * 2;
@@ -39,8 +44,8 @@ namespace Bin.Map
         {
             _grid = new Node[_gridSizeX, _gridSizeY];
             var worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 -
-                                  Vector3.forward * gridWorldSize.y / 2; 
-            
+                                  Vector3.forward * gridWorldSize.y / 2;
+
             for (var x = 0; x < _gridSizeX; x++)
             {
                 for (var y = 0; y < _gridSizeY; y++)
@@ -51,19 +56,83 @@ namespace Bin.Map
                     var walkable = !Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask);
                     var movementPenalty = 0;
 
-                    if (walkable)
+
+                    var ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, 100, _walkableLayerMask))
                     {
-                        var ray = new Ray(worldPoint + Vector3.up *50, Vector3.down);
-                        RaycastHit hit;
-                        if (Physics.Raycast(ray, out hit, 100, _walkableLayerMask))
-                        {
-                            _walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
-                        }
+                        _walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
+                    }
+
+                    if (!walkable)
+                    {
+                        movementPenalty += obstacleProximityPenalty;
                     }
 
                     _grid[x, y] = new Node(walkable, worldPoint, x, y, movementPenalty);
                 }
             }
+
+            BlurPenaltyMap(3);
+        }
+
+        private void BlurPenaltyMap(int blurSize)
+        {
+            var kernelSize = blurSize * 2 + 1;
+            var kernelExtents = (kernelSize - 1) / 2;
+            var penaltyHorizontalPass = new int[_gridSizeX, _gridSizeY];
+            var penaltyVerticalPass = new int[_gridSizeX, _gridSizeY];
+
+            for (var y = 0; y < _gridSizeY; y++)
+            {
+                for (var x = -kernelExtents; x <= kernelExtents; x++)
+                {
+                    var sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                    penaltyHorizontalPass[0, y] += _grid[sampleX, y].MovementPenalty;
+                }
+
+                for (var x = 1; x < _gridSizeX; x++)
+                {
+                    var removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, _gridSizeX);
+                    var addIndex = Mathf.Clamp(x + kernelExtents, 0, _gridSizeX - 1);
+
+                    penaltyHorizontalPass[x, y] = penaltyHorizontalPass[x - 1, y] -
+                        _grid[removeIndex, y].MovementPenalty + _grid[addIndex, y].MovementPenalty;
+                }
+            }
+            
+            for (var x = 0; x < _gridSizeY; x++)
+            {
+                for (var y = -kernelExtents; y <= kernelExtents; y++)
+                {
+                    var sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                    penaltyVerticalPass[x, 0] += penaltyHorizontalPass[x, sampleY];
+                }
+
+                var blurredPenalty = Mathf.RoundToInt((float)penaltyVerticalPass[x, 0] / (kernelSize * kernelSize));
+                _grid[x, 0].MovementPenalty = blurredPenalty;
+                
+                for (var y = 1; y < _gridSizeY; y++)
+                {
+                    var removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, _gridSizeY);
+                    var addIndex = Mathf.Clamp(y + kernelExtents, 0, _gridSizeY - 1);
+
+                    penaltyVerticalPass[x, y] = penaltyVerticalPass[x, y - 1] -
+                        penaltyHorizontalPass[x, removeIndex] + penaltyHorizontalPass[x, addIndex];
+                    blurredPenalty = Mathf.RoundToInt((float)penaltyVerticalPass[x, y] / (kernelSize * kernelSize));
+                    _grid[x, y].MovementPenalty = blurredPenalty;
+
+                    if (blurredPenalty > _penaltyMax)
+                    {
+                        _penaltyMax = blurredPenalty;
+                    }
+                    if (blurredPenalty < _penaltyMin)
+                    {
+                        _penaltyMin = blurredPenalty;
+                    }
+                }
+            }
+            
         }
 
         public List<Node> GetNeighbours(Node node)
@@ -111,8 +180,11 @@ namespace Bin.Map
             {
                 foreach (var node in _grid)
                 {
-                    Gizmos.color = node.Walkable ? Color.white : Color.red;
-                    Gizmos.DrawCube(node.WorldPosition, Vector3.one * (_nodeDiameter - .1f));
+                    Gizmos.color = Color.Lerp(Color.white, Color.black,
+                        Mathf.InverseLerp(_penaltyMin, _penaltyMax, node.MovementPenalty));
+                    
+                    Gizmos.color = node.Walkable ? Gizmos.color : Color.red;
+                    Gizmos.DrawCube(node.WorldPosition, Vector3.one * (_nodeDiameter));
                 }
             }
         }
