c6ca5417b5e1e4c18cd78d3f850abc06fb1b00dd
Rostov <rostov.90.100@gmail.com>
2021-08-31 08:48:24 +0300

    "feature_PlayerInputController[started]"

---
+++ Assets/Bin/Player/InputManager.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+namespace Bin.Player
+{
+    public class InputManager : MonoBehaviour
+    {
+        private InputListener _inputListener;
+
+        private void Awake()
+        {
+            _inputListener = new InputListener();
+        }
+
+        private void OnGUI()
+        {
+            _inputListener.OnAction(Event.current);
+        }
+    }
+}
